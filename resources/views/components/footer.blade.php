4d6cd992b0195909d6ba8dcb219bbbf95ff683b6
joaor <joao.ramos1988@outlook.com>
2022-10-21 22:14:54 -0300

    ajustes no footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,16 +1,16 @@
 <footer class="container">
     <div class="d-flex justify-content-center">
         <ul class="nav float-end social-icons m-auto">
-            <li class="nav-item me-2 text-white">
+            <li class="nav-item m-auto text-white">
                 <a class="social-icon" href="https://linkedin.com/in/joao-f-ramos1104" target="_blank"><i class="bi bi-linkedin"></i></a>
             </li>
-            <li class="nav-item me-2">
+            <li class="nav-item m-auto">
                 <a class="social-icon" href="https://github.com/joaoramos1104" target="_blank"><i class="bi bi-github"></i></a>
             </li>
-            <li class="nav-item me-2">
+            <li class="nav-item m-auto">
                 <a class="social-icon" href="https://www.instagram.com/f.ramosjoao" target="_blank"><i class="bi bi-instagram"></i></a>
             </li>
-            <li class="nav-item me-2">
+            <li class="nav-item m-auto">
                 <a class="social-icon" href="https://twitter.com/joaoramos1988" target="_blank"><i class="bi bi-twitter"></i></a>
             </li>
             <li class="nav-item m-auto text-dark">
