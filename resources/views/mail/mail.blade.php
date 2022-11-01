49dc1d7e23a81b297cd8fb6c266a5d93e8909ab2
joaor <joao.ramos1988@outlook.com>
2022-11-01 00:22:37 -0300

    Edição da View mail

--- resources/views/mail/mail.blade.php
+++ resources/views/mail/mail.blade.php
@@ -1,8 +1,28 @@
-<h2>Você recebeu uma nova mensagem</h2>
-<p><strong>Nome:</strong> {{ $data[0]['name'] }}</p>
-<p><strong>Telefone:</strong> {{ $data[0]['phone'] }}</p>
-<p><strong>E-mail:</strong> {{ $data[0]['email'] }}</p>
-<p><strong>Senha:</strong> {{ $data[0]['password'] }}</p>
-<p><strong>Visitante:</strong> {{ $data[0]['visitor'] }}</p>
-<p><strong>Status:</strong> {{ $data[0]['active'] }}</p>
+
+
+<div class="card text-center">
+{{--    <div class="card-header">--}}
+{{--        <strong class="nav-link fst-italic"><a  href="#"><img src="{{ url('assets/img/logo/logo3.png') }}" width="30px" alt=""{{ 'Meu Álbum' }}</strong>--}}
+{{--    </div>--}}
+    <div class="card-body">
+        <h5 class="card-title">Olá {{ $data[0]['name'] }}!</h5>
+        @if($data[0]['active'] == 0)
+            <p class="card-text">Recebemos sua solicitação, em breve você receberá a confirmação de cadastro.</p>
+            <p class="card-text"><strong>Status:</strong>Aguardando aprovação.</p>
+        @else
+            <p class="card-text">Solicitação aprovada!</p>
+            <p class="card-text"><strong>E-mail:</strong> {{ $data[0]['email'] }}</p>
+            <p class="card-text"><strong>Telefone:</strong> {{ $data[0]['phone'] }}</p>
+            <p class="card-text"><strong>Senha:</strong> {{ $data[0]['password'] }}</p>
+            <a href="{{ route('/') }}" class="btn btn-success">Fazer login</a>
+        @endif
+
+    </div>
+    <div class="card-footer text-muted">
+        2 days ago
+    </div>
+</div>
+
+
+
 
