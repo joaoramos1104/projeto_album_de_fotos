1f30091a328ed2705564e22e3fe7193696dec0f4
joaor <joao.ramos1988@outlook.com>
2023-04-25 21:54:09 -0300

    Melhora visual e navegação

--- database/migrations/2022_10_01_153910_create_albums_table.php
+++ database/migrations/2022_10_01_153910_create_albums_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('albums', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('descricao');
             $table->string('capa_album');
             $table->timestamps();
         });
--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -3,10 +3,18 @@
 @section('content')
     <div class="container">
         <div id="carouselCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel">
-            <div class="carousel-indicators">
+            <div class="carousel-indicators indicator">
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="text-dark"><i class="bi bi-arrow-left-circle"></i></span>
+                </button>
                 @foreach($albums as $key => $value)
                 <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="{{ $key }}" class="{{$key == 0 ? 'active' : '' }}" aria-current="true" aria-label="Slide{{ $key }}"></button>
                 @endforeach
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="text-dark"><i class="bi bi-arrow-right-circle"></i></span>
+                </button>
             </div>
             <div class="carousel-inner">
             @foreach($albums as $key => $album)
@@ -17,18 +25,19 @@
                         </div>
                         <div class="col-md-4 col-sm-12 float-end m-auto text-center p-3">
                             <h1 class="fw-bold" style="font-family: Calligraffitti;">Álbum : {{ $album->name }}</h1>
+                            <p>{{ $album->descricao }}</p>
                             <a class="btn btn-sm btn-light mb-1 shadow" href="{{ route('photos_album', $album->id) }}" >Ver Fotos</a>
                         </div>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselCaptions" data-bs-slide="prev">
+                {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselCaptions" data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
                 </button>
                 <button class="carousel-control-next" type="button" data-bs-target="#carouselCaptions" data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
-                </button>
+                </button> --}}
             @endforeach
         </div>
     </div>
